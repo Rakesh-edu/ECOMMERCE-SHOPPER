3db34e3196c10b3a56dcc6425202241fafce77c2
Rakesh-edu <rakeshbanavath5@gmail.com>
2024-09-05 18:00:24 +0530

    A ecommerce website with a good user experience

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigator'>
+            <div className='descriptionbox-nav-box'>Description</div>
+            <div className='descriptionbox-nav-box fade'>Reviews (122)</div>
+            </div>
+            <div className='descriptionbox-description'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam maiores rem quo perspiciatis quos nihil magnam nobis magni at repellendus sint consequatur deserunt optio amet, obcaecati odit beatae voluptatem quisquam?
+                </p>
+            </div>
+        
+    </div>
+  )
+}
+
+export default DescriptionBox
